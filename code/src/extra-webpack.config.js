193f5a9a4577a9911506b318dacea44db92228a5
chybie <chy.bie@hotmail.com>
2019-01-01 03:17:52 +0000

    fix prerender

--- code/src/extra-webpack.config.js
+++ code/src/extra-webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
-// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 
 // ... add in your webpack plugins
@@ -8,9 +8,13 @@ module.exports = {
     plugins: [
         new PrerenderSPAPlugin({
             // Required - The path to the webpack-outputted app to prerender.
-            staticDir: path.join(__dirname, '../../year-in-review/2018'),
+            staticDir: path.join(__dirname, '../../'),
             // Required - Routes to render.
-            routes: ['/'],
+            routes: ['/year-in-review/2018'],
+            indexPath: path.join(__dirname, '../../year-in-review/2018', 'index.html')
+            // renderer: new Renderer({
+            //     renderAfterElementExists: 'app-helper-list'
+            // })
         })
     ]
 }
